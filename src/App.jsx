8dda52914d3d8d7e696b8cd217622ffc4fce8566
Yiting Xiao <yitingxiao0531@gmail.com>
2024-06-25 19:58:22 -0400

    refactor: delete redundant css file for appjs

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,20 @@ import CreateVideo from './components/CreateVideo/CreateVideo';
 import VideoList from './components/VideoList/VideoList';
 import NavBar from './components/NavBar/NavBar';
 import PlayVideo from './components/PlayVideo/PlayVideo';
-import Profile from './components/Profile/Profile'; 
-import styles from './App.module.css'; 
+import Profile from './components/Profile/Profile';
 
 function App() {
   return (
     <VideoProvider>
-      <div className={styles.appContainer}>
-        <BrowserRouter>
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<VideoList />} />
-            <Route path="/create" element={<CreateVideo />} />
-            <Route path="/playvideo/:videoId" element={<PlayVideo />} />
-            <Route path="/profile" element={<Profile />} /> {/* add Profile route */}
-          </Routes>
-        </BrowserRouter>
-      </div>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<VideoList />} />
+          <Route path="/create" element={<CreateVideo />} />
+          <Route path="/playvideo/:videoId" element={<PlayVideo />} />
+          <Route path="/profile" element={<Profile />} /> {/* add Profile route */}
+        </Routes>
+      </BrowserRouter>
     </VideoProvider>
   );
 }
